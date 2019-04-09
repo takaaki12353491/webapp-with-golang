6c3bc6a8eee6dd7a12b17c5cbd877de2ead44b4b
takaaki <takaaki@worldergroup.com>
2019-04-09 14:26:38 +0900

    Output chat log

--- chat/main.go
+++ chat/main.go
@@ -5,8 +5,10 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+	"trace"
 )
 
 // templは１つのテンプレートを表します
@@ -28,6 +30,7 @@ func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse() //フラグを解釈します
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
 	// チャットルームを開始します
